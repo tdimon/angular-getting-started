ad676a45861b0972608654e8e5e341ad8b531c75
tdimon <30657136+tdimon@users.noreply.github.com>
2023-02-01 00:46:44 +0300

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,7 +2,18 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CartService {
+  items = [];
 
-  constructor() { }
+  addToCart(product) {
+    this.items.push(product);
+  }
 
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
